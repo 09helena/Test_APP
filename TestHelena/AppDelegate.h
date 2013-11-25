b3810457c07fb390be6b4518a2d1da83b8eb8887
Helena Wingert <09helena@web.de>
2013-11-25 09:20:11 +0100

    Initial Commit

---
+++ TestHelena/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestHelena
+//
+//  Created by appdev on 25.11.13.
+//  Copyright (c) 2013 FH SWF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
