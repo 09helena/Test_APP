646e26785c8c89453ef968027a4aa94f826e419a
Helena Wingert <09helena@web.de>
2013-11-25 09:23:48 +0100

    Ich habe ein Fehler eingebaut Helena

---
+++ TestHelena/ddjkfjTest.h
@@ -0,0 +1,13 @@
+//
+//  ddjkfjTest.h
+//  TestHelena
+//
+//  Created by appdev on 25.11.13.
+//  Copyright (c) 2013 FH SWF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ddjkfjTest : NSObject
+
+@end
---
+++ TestHelena/ddjkfjTest.m
@@ -0,0 +1,13 @@
+//
+//  ddjkfjTest.m
+//  TestHelena
+//
+//  Created by appdev on 25.11.13.
+//  Copyright (c) 2013 FH SWF. All rights reserved.
+//
+
+#import "ddjkfjTest.h"
+
+@implementation ddjkfjTest
+
+@end
